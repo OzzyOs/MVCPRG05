77d273c7dc1bb7dd9cbf99d4b1d02288593a6e7d
OzzyOs <Joshahursepuny@gmail.com>
2024-10-10 14:25:38 +0200

    Add : Routes Add : ContactController Add : Named Routes Edit : CHANGELOG.md

--- _changelog/CHANGELOG.md
+++ _changelog/CHANGELOG.md
@@ -1,6 +1,13 @@
-09/10/2024 : 
-Add : Local Repo & Github Repo
+# 10/10/2024 :
+* Yugioh TCG ?
+* Add ContactController
+* Use ContactController to pass parameters in Routes
 
-10/10/2024 :
-Add : Changelog.md
-Add : _changelog directory
+# 10/10/2024 :
+* Add : Changelog.md
+* Add : _changelog directory
+
+# 09/10/2024 : 
+* Add : Local Repo & Github Repo
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+!!  // route aanmaken met gebruik van controller
+Route ::get('/contact', [ContactController::class, 'show' ])->name('dodo');
+    // turn it into a named route
+
+
+
+
+
+// passeer parameters via de url
+Route::get('products/{id}', function(string $id){
+    return view('product-id', [
+        'id'=>$id,
+    ]);
 });
 
 Route::get('/dashboard', function () {
